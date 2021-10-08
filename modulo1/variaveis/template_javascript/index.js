9e6cbedd6e2371ac46aa5c43d567c5f9f9738b33
Fernando-Orsini <fernandotaveiraorsini@hotmail.com>
2021-10-08 19:02:35 -0400

    variaveis inacadas

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,4 @@
+//questoes leitura de codigo 
+//1) sera impresso na tela o valor 10 e 5
+//2) sera impresso 20, null, null
+//3) primeira variavel, eu renomearia let cargaHorariaDiaria. a segunda  
