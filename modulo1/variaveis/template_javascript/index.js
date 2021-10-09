92fa68cbf45a1c4f03a7f5edea736bf7c0750e05
Fernando-Orsini <fernandotaveiraorsini@hotmail.com>
2021-10-08 21:56:43 -0400

    projeto de fixacao incompleto

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,13 @@
+//questoes leitura de codigo 
+//1) sera impresso na tela o valor 10 e 5
+//2) sera impresso 20, null, null
+//3) primeira variavel, eu renomearia let cargaHorariaDiaria. a segunda  let remuneracaoDiaria
+
+
+
+//questoes escrita de codigo
+const nome = []
+const idade = []
+typeof nome 
+tt
+
---
+++ projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -0,0 +1,144 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  // implemente sua lógica aqui
+  return num1 + num2
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  // implemente sua lógica aqui
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo() {
+  // const resultado = altura * largura
+  // return resultado
+
+  //Exemplo1  
+function calculaAreaRetangulo(3, 5)
+const resultado = altura * largura
+altura = 3
+largura = 5
+return resultado
+
+//Exemplo 2
+function calculaAreaRetangulo(2, 6)
+const resultado = altura * largura
+altura = 2
+largura = 6
+return resultado
+
+}
+
+// EXERCÍCIO 02
+function imprimeIdade(dataAtual - dataDeNascimento) {
+  // const resultado = dataAtual - dataDeNascimento
+  // return resultado
+
+  //Exemplo 1
+  function imprimeIdade(dataAtual - dataDeNascimento) {
+  const dataAtual = 2020
+  const dataDeNascimento = 1990
+  const resultado = dataAtual - dataDeNascimento
+  return resultado 
+
+}
+
+// EXERCÍCIO 03
+function calculaIMC(peso, altura) {
+const IMC = peso / altura * altura
+return IMC
+//Expemplo 1
+function calculaIMC(peso, altura) {
+const IMC = peso / altura * altura
+const peso = 85
+const altura = 1.8
+return IMC
+
+//Exexmplo 2
+function calculaIMC(peso, altura) {
+  const IMC = peso / altura * altura
+  const peso = 70
+  const altura = 1.65
+  return IMC
+}
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+  // implemente sua lógica aqui
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
