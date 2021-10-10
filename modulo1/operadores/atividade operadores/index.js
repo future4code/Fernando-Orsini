20b889db9db47643d57ceb2662403148eb95f5d9
Fernando-Orsini <fernandotaveiraorsini@hotmail.com>
2021-10-10 18:39:03 -0400

    atvidade de operadores completa

---
+++ modulo1/operadores/atividade operadores/index.js
@@ -0,0 +1,74 @@
+// Exercícios de interpretação de código
+// 1) const bool1 = true
+//const bool2 = false
+//const bool3 = !bool2  resultado = bool3 = true
+
+//let resultado = bool1 && bool2
+//console.log("a. ", resultado FALSE)
+
+//resultado = bool1 && bool2 && bool3 
+//console.log("b. ", resultado FALSE) 
+
+//resultado = !resultado && (bool1 || bool2) 
+//console.log("c. ", FALSE)
+
+//console.log("d. ", typeof resultado Booleano)
+
+
+//2)
+//let primeiroNumero = prompt("Digite um numero!")
+//let segundoNumero = prompt("Digite outro numero!")
+
+//const soma = let primeiroNumero + let segundoNumero
+
+//console.log(soma)
+
+
+//Exercícios de escrita de código
+//1)
+let idade = prompt("Qual a sua idade?")
+let idadeDoMelhorAmigo = prompt("Qual a idade do seu melhor amigo?")
+console.log("Sua idade é maior do que a do seu melhor amigo?") = true
+let idade = 34
+let idadeDoMelhorAmigo= 30
+const resultado = idade - idadeDoMelhorAmigo 
+console.log(resultado)
+
+
+//2)
+//a) peça um número par
+let numeroPar = prompt("Digite um numero par")
+let numeroPar = 8
+const restoDaDivisao = 8 % 2
+console.log(restoDaDivisao)
+
+//3)
+
+let idadeEmAnos = prompt("Quantos anos você tem")
+let idadeEmanos = 34
+const mesesEmUmAno = 12
+let idadeEmMeses = idadeEmAnos * mesesEmUmAno)
+let idadeEmMeses = 408
+const diasEmUmAno = 365
+let idadeEmDias =  idadeEmMeses * diasEmUmAno
+console.log(idadeEmDias)
+//resultado= 148920
+const horasEmUmdia = 24
+let idadeEmHoras = idadeEmDias * horasEmUmdia
+//resultado = 3574080
+
+//4)
+let primeiroNumero = prompt("Digite o primeiro numero")
+let segundoNumero = prompt("Digite o segundo numero")
+let primeiroNumero = 10
+let segundoNumero = 5
+let 10 > 5 = true
+let 10 === 5 = false
+const primeiraDivisao = primeiroNumero / segundoNumero
+const primeiraDivisao = true
+//Resultado 2
+const segundaDivisao = segundoNumero / primeiroNumero
+const segundaDivisao = true
+//Resultado 0,5
+
+
