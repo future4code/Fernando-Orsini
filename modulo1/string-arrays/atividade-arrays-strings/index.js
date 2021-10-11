488e7a109797d7b97b34c5dd13806e4067baa171
Fernando-Orsini <fernandotaveiraorsini@hotmail.com>
2021-10-10 22:00:59 -0400

    atividade de strings-arrays completa

---
+++ modulo1/string-arrays/atividade-arrays-strings/index.js
@@ -0,0 +1,55 @@
+//Exercícios de interpretação de código
+//1)
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+console.log(array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
+const valor = 24
+
+//2)
+const frase = prompt("Digite uma frase")
+const frase = "Subi num ônibus em Marrocos"
+console.log(frase.length) = 27
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+const fraseMaiscula = "SUBI NUM ÔNIBUS EM MIRROCOS"
+
+
+//Exercícios de escrita de código
+//1)
+const nomeDoUsuario = prompt("Digite o seu nome")
+const nomeDoUsuario = Fernando
+const emailDoUsuario = prompt("Digite o seu email")
+const emailDoUsuario = fernandotaveiraorsini@hotmail.com
+const frase = "O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. " + " Seja bem-vinda(o), " + nomeDoUsuario + " ! "
+console.log(frase)
+
+//2)
+const comidasPreferidas = ["batata", "pizza", "queijo","chocolate", "pão"]
+console.log(comidasPreferidas)
+console.log("Essas são as minhas comidas preferidas")
+//B e C não sei como fazer
+
+//3)
+
+let listaDeTarefas
+let primeiraTarefa = prompt("Digite a primeiraTarefa")
+let segundaTarefa = prompt("Digite a segundaTarefa")
+let terceiraTarefa = prompt("Digite a terceiraTarefa")
+let listaDeTarefas = [primeiraTarefa + segundaTarefa + terceiraTarefa]
+console.log(listaDeTarefas)
+let listaDeTarefas = prompt("Digite qual tarefa já foi realizada")
+listaDeTarefas.splice(i, n) 
