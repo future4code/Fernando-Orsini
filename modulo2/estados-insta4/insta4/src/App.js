e9a94cf1f24a971b0545600e79d2b5b3658d8fa4
Fernando-Orsini <fernandotaveiraorsini@hotmail.com>
2021-11-04 21:06:30 -0400

    Insta4

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+<Post
+          nomeUsuario={'hendrix'}
+          fotoUsuario={'https://upload.wikimedia.org/wikipedia/commons/a/ae/Jimi_Hendrix_1967.png'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+ <Post
+          nomeUsuario={'lenon'}
+          fotoUsuario={'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/John_Lennon_abbey_road.jpg/453px-John_Lennon_abbey_road.jpg'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+      
+
+      </MainContainer>
+
+
+      
+
+      
+    );
+  }
+}
+
+export default App;
