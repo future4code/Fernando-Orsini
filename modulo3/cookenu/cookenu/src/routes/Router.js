aa0404d8ffad7a1006481f54653af0e7d4a7eb30
Fernando-Orsini <fernandotaveiraorsini@hotmail.com>
2021-12-13 23:00:42 -0400

    Código até o Header sem erros

--- modulo3/cookenu/cookenu/src/routes/Router.js
+++ modulo3/cookenu/cookenu/src/routes/Router.js
@@ -6,10 +6,12 @@ import RecipesDetailPage from '../pages/RecipesDetailPage/RecipesDetailPage';
 import RecipesListPage from '../pages/RecipesListPage/RecipesListPage'
 import SignUpPage from '../pages/SignUpPage/SignUpPage';
 import ErrorPage from '../pages/ErrorPage/ErroPage';
+import Header from '../components/Header/Header';
 
 const Router = () => {
     return (
        <BrowserRouter>
+       <Header/>
        <Switch>
            <Route exact path="/login">
             <LoginPage/>
