ced4504cd5a46e768b8a6990eef4fc53bb182c3b
FernandoOrsini <fernandotaveiraorsini@hotmail.com>
2022-02-15 05:47:42 -0300

    atividade imcompleta

---
+++ modulo4/filtros-ordenacao-paginacao/template-filtros-ordenacao-paginacao/src/data/selectUsersByType.ts
@@ -0,0 +1,12 @@
+import connection from "./connection";
+
+export default async function selectAllUsers(
+    type: string
+    
+):Promise<any> {
+    const result = await connection('aula48_exercicio')
+         .where({ type: type });
+
+    return result;
+    
+};
